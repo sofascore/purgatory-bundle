1cd44411ae5c2365272e5e57e3f75c483399aee8
Ante Šušak <ante.susak7@gmail.com>
2021-06-14 14:43:49 +0200

    rename extension to match bundle name

--- DependencyInjection/PurgatoryExtension.php
+++ DependencyInjection/PurgatoryExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class SofaScoreCacheRefreshExtension extends Extension
+class PurgatoryExtension extends Extension
 {
     /**
      * {@inheritDoc}
