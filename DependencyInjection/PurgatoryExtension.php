a98179d9cd93fef12fc46c1036c2776a423c46b4
Petar Obradović <petar.obradovic@sofascore.com>
2021-08-12 15:44:18 +0200

    Move purger configuration logic to a compiler pass

--- DependencyInjection/PurgatoryExtension.php
+++ DependencyInjection/PurgatoryExtension.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace SofaScore\Purgatory\DependencyInjection;
 
-use SofaScore\Purgatory\Purger\PurgerInterface;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -23,23 +21,10 @@ final class PurgatoryExtension extends Extension
 
         $config = $this->processConfiguration(new Configuration(), $configs);
 
-        $this->configurePurgerService($container, $config['purger']);
+        $container->setParameter('sofascore.purgatory.purger', $config['purger']);
 
         if (!$config['entity_change_listener']) {
             $container->removeDefinition('sofascore.purgatory.entity_change_listener');
         }
-    }
-
-    private function configurePurgerService(ContainerBuilder $container, string $serviceId): void
-    {
-        $webCacheImplementation = $container->getDefinition($serviceId);
-
-        if (!is_a($webCacheImplementation->getClass(), PurgerInterface::class, true)) {
-            throw new \LogicException(
-                sprintf('The purger service should implement the \'%s\' interface.', PurgerInterface::class)
-            );
-        }
-
-        $container->setAlias('sofascore.purgatory.purger', new Alias($serviceId, false));
     }
 }
