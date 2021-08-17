7ffa363b02e0f65b9df7274f2556f101619c72bc
Matko Andjelinic <matko.andjelinic@gmail.com>
2021-08-17 12:39:44 +0200

    more clear order of attributes and annotations handling statements

--- Mapping/Loader/AnnotationsLoader.php
+++ Mapping/Loader/AnnotationsLoader.php
@@ -187,8 +187,8 @@ class AnnotationsLoader implements LoaderInterface, WarmableInterface
         [$controller, $method] = $controllerCallable;
 
         $reflectionMethod = new \ReflectionMethod($controller, $method);
-        $methodAnnotations = $this->annotationReader->getAnnotations($reflectionMethod);
 
+        // handle php8 attributes
         if (\PHP_VERSION_ID >= 80000 && count($attributes = $reflectionMethod->getAttributes(SubscribeTo::class, \ReflectionAttribute::IS_INSTANCEOF)) > 0) {
             foreach ($attributes as $attribute) {
                 $annotation = $attribute->newInstance();
@@ -196,6 +196,8 @@ class AnnotationsLoader implements LoaderInterface, WarmableInterface
             }
         }
 
+        // handle annotations
+        $methodAnnotations = $this->annotationReader->getAnnotations($reflectionMethod);
         foreach ($methodAnnotations as $class => $annotations) {
             if ($class === SubscribeTo::class) {
                 foreach ($annotations as $annotation) {
