37732ca32fb8b3945c01f85658b20c10d6b2066b
Matko Andjelinic <matko.andjelinic@gmail.com>
2021-08-19 21:46:07 +0200

    tests for dual driver

---
+++ tests/AnnotationReader/Fixtures/SillyLittleAnnotation.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AnnotationReader\Fixtures;
+
+/**
+ * @Annotation
+ */
+#[\Attribute(\Attribute::TARGET_ALL)]
+class SillyLittleAnnotation
+{
+    private ?string $value;
+
+    public function __construct($value = null)
+    {
+        if (is_array($value)) {
+            $this->value = $value['value'] ?? null;
+        } elseif (is_string($value)) {
+            $this->value = $value;
+        } else {
+            throw new \TypeError('Expected string or array');
+        }
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+}
