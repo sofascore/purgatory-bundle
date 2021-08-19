a84885a39861eb079be8db6e1920ea90a7dad42c
Matko Andjelinic <matko.andjelinic@gmail.com>
2021-08-19 15:11:47 +0200

    Properties attribute

--- Annotation/Properties.php
+++ Annotation/Properties.php
@@ -6,13 +6,14 @@ namespace SofaScore\Purgatory\Annotation;
  * @Annotation
  * @Target("METHOD")
  */
+#[\Attribute(\Attribute::TARGET_METHOD)]
 final class Properties
 {
     private array $properties;
 
-    public function __construct(array $values)
+    public function __construct(array $value = [])
     {
-        $this->properties = (array) ($values['value'] ?? []);
+        $this->properties = $value['value'] ?? $value ?? [];
     }
 
     public function getProperties(): array
