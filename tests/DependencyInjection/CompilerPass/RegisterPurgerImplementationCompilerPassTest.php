4692f3906bdbf7355dc1cea23b50afaa0f147cbf
Domagoj Polović <Vorta@users.noreply.github.com>
2021-09-02 12:18:42 +0200

    [CompilerPass:Tests] Added Compiler Pass tests (#25)

---
+++ tests/DependencyInjection/CompilerPass/RegisterPurgerImplementationCompilerPassTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DependencyInjection\CompilerPass;
+
+use PHPUnit\Framework\TestCase;
+use SofaScore\Purgatory\DependencyInjection\CompilerPass\RegisterPurgerImplementationCompilerPass;
+use SofaScore\Purgatory\Purger\PurgerInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+/**
+ * @covers \SofaScore\Purgatory\DependencyInjection\CompilerPass\RegisterPurgerImplementationCompilerPass
+ */
+class RegisterPurgerImplementationCompilerPassTest extends TestCase
+{
+    private ContainerBuilder $container;
+    private RegisterPurgerImplementationCompilerPass $compilerPass;
+
+    protected function setUp(): void
+    {
+        $this->container = new ContainerBuilder();
+        $this->compilerPass = new RegisterPurgerImplementationCompilerPass();
+    }
+
+    public function testCompilerPassProcess(): void
+    {
+        $this->container->setParameter('sofascore.purgatory.purger', 'purger_test');
+
+        self::assertTrue($this->container->hasParameter('sofascore.purgatory.purger'));
+        self::assertFalse($this->container->hasAlias('sofascore.purgatory.purger'));
+
+        $this->container->addDefinitions(['purger_test' => new Definition(PurgerInterface::class)]);
+        $this->compilerPass->process($this->container);
+
+        self::assertFalse($this->container->hasParameter('sofascore.purgatory.purger'));
+        self::assertTrue($this->container->hasAlias('sofascore.purgatory.purger'));
+    }
+}
---
+++ tests/DependencyInjection/CompilerPass/SymfonyPurgerRemovalCompilerPassTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DependencyInjection\CompilerPass;
+
+use PHPUnit\Framework\TestCase;
+use SofaScore\Purgatory\DependencyInjection\CompilerPass\SymfonyPurgerRemovalCompilerPass;
+use SofaScore\Purgatory\Purger\SymfonyPurger;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+/**
+ * @covers \SofaScore\Purgatory\DependencyInjection\CompilerPass\SymfonyPurgerRemovalCompilerPass
+ */
+class SymfonyPurgerRemovalCompilerPassTest extends TestCase
+{
+    private ContainerBuilder $container;
+    private SymfonyPurgerRemovalCompilerPass $compilerPass;
+
+    protected function setUp(): void
+    {
+        $this->container = new ContainerBuilder();
+        $this->compilerPass = new SymfonyPurgerRemovalCompilerPass();
+    }
+
+    public function testCompilerPassProcessWithoutExistingHttpCache(): void
+    {
+        $this->container->addDefinitions(['sofascore.purgatory.purger.symfony' => new Definition(SymfonyPurger::class)]);
+
+        self::assertTrue($this->container->hasDefinition('sofascore.purgatory.purger.symfony'));
+
+        $this->compilerPass->process($this->container);
+
+        self::assertFalse($this->container->hasDefinition('sofascore.purgatory.purger.symfony'));
+    }
+
+    public function testCompilerPassProcessWithExistingHttpCache(): void
+    {
+        $this->container->addDefinitions([
+            'http_cache.store' => new Definition('TestClass'),
+            'sofascore.purgatory.purger.symfony' => new Definition(SymfonyPurger::class)
+        ]);
+
+        self::assertTrue($this->container->hasDefinition('sofascore.purgatory.purger.symfony'));
+
+        $this->compilerPass->process($this->container);
+
+        self::assertTrue($this->container->hasDefinition('sofascore.purgatory.purger.symfony'));
+    }
+}
