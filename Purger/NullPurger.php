138de8f94e46c6205358597ee5bf682decde9620
zlatkoverk <zlatko112@gmail.com>
2021-09-20 12:14:18 +0200

    Rename to null purger

--- Purger/NullPurger.php
+++ Purger/NullPurger.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace SofaScore\Purgatory\Purger;
 
 
-final class NoActionPurger implements PurgerInterface
+final class NullPurger implements PurgerInterface
 {
     public function purge(iterable $urls): void
     {
