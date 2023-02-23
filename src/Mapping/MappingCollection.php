af71ee6637652e0d5e4ae3c18b14a8b993c3df07
Berislav Balogović <berislav.balogovic@sofascore.com>
2023-02-23 22:54:29 +0100

    Refactor CI

--- src/Mapping/MappingCollection.php
+++ src/Mapping/MappingCollection.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Sofascore\PurgatoryBundle\Mapping;
 
+/**
+ * @template-implements \IteratorAggregate<string, MappingValue[]>
+ */
 class MappingCollection implements \IteratorAggregate, \Countable
 {
     /**
