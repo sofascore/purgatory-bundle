a4de838950e57cdde936f406d9dfbc81fab9c456
Adrian Brajković <adrian.brajkovic@sofascore.com>
2025-04-22 20:24:19 +0200

    add tests

---
+++ tests/Maker/Util/PurgeOnYamlBuilderTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sofascore\PurgatoryBundle\Tests\Maker\Util;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Sofascore\PurgatoryBundle\Maker\Util\PurgeOnYamlBuilder;
+
+#[CoversClass(PurgeOnYamlBuilder::class)]
+final class PurgeOnYamlBuilderTest extends TestCase
+{
+    // TODO
+
+    public function testDummy(): void
+    {
+        self::assertTrue(true);
+    }
+}
