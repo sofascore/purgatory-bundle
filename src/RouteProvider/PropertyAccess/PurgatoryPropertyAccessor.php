3dfd226eb8bffef1a442e927d42657be67829ca1
kresimir.lezaic <kresimir.lezaic@sofascore.com>
2025-07-01 14:56:54 +0200

    Static code analysis fix

--- src/RouteProvider/PropertyAccess/PurgatoryPropertyAccessor.php
+++ src/RouteProvider/PropertyAccess/PurgatoryPropertyAccessor.php
@@ -78,6 +78,8 @@ final class PurgatoryPropertyAccessor implements PropertyAccessorInterface
     /**
      * @param object|array<array-key, mixed> $objectOrArray
      * @param string|PropertyPathInterface   $propertyPath
+     *
+     * @param-out object|array<array-key, mixed> $objectOrArray
      */
     public function setValue(&$objectOrArray, $propertyPath, mixed $value): void
     {
@@ -107,7 +109,7 @@ final class PurgatoryPropertyAccessor implements PropertyAccessorInterface
             $this->getValue($objectOrArray, $propertyPath);
 
             return true;
-        } catch (AccessException|UnexpectedTypeException) {
+        } catch (PropertyNotAccessibleException) {
             return false;
         }
     }
